d39f8601c7cefdfb64e04bef000100e183acf840
Gerald Versluis <gerald@verslu.is>
2018-04-06 18:31:22 +0200

    Initial commit

---
+++ CfpExchange/Controllers/CfpController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CfpExchange.Controllers
+{
+	public class CfpController : Controller
+	{
+		public IActionResult Submit()
+		{
+			return View();
+		}
+
+		public IActionResult Details(Guid id)
+		{
+			return View();
+		}
+	}
+}
---
+++ CfpExchange/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CfpExchange.Models;
+
+namespace CfpExchange.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
