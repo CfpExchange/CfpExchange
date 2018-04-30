55735d4aea21ec1c02d86ad576b06631107ad0e3
Pat Hermens <p.hermens@coolblue.nl>
2018-04-30 23:05:11 +0200

    Adding strongly-typed EmailSettings class

---
+++ CfpExchange/Configuration/EmailSettings.cs
@@ -0,0 +1,7 @@
+public class EmailSettings
+{
+    public string ApiKey { get; set; }
+    public string ApiBaseUri { get; set; }
+    public string RequestUri { get; set; }
+    public string From { get; set; }
+}
