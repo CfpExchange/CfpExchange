0bb54ed42ef20956cc07055ae0945aa52c65e27f
Jan de Vries <jan@jan-v.nl>
2018-08-01 22:34:35 +0200

    initial version for downloading image from remote location and storing it in blob

--- CfpExchange.Functions/DownloadEventImage.cs
+++ CfpExchange.Functions/DownloadEventImage.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.ServiceBus.Messaging;
@@ -7,11 +11,27 @@ namespace CfpExchange.Functions
     public static class DownloadEventImage
     {
         [FunctionName("DownloadEventImage")]
-        public static void Run(
+        public static async Task Run(
             [ServiceBusTrigger("eventimages", AccessRights.Manage, Connection = "ServicebusQueueConnectionString")]
-            string myQueueItem, TraceWriter log)
+            Model.DownloadEventImage eventImageModel,
+            Binder binder,
+            TraceWriter log)
         {
-            log.Info($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            log.Info($"Processing the download event image for identifier `{eventImageModel.Id}`");
+
+            Uri uri = new Uri(eventImageModel.ImageUrl);
+            var filename = Path.GetFileName(uri.LocalPath);
+            var downloadLocationForEventImage = $"event-images/{eventImageModel.Id}/{filename}";
+
+            using (var blobBinding = await binder.BindAsync<BinaryWriter>(
+                                                new BlobAttribute(downloadLocationForEventImage, FileAccess.Write)))
+            {
+                var webClient = new WebClient();
+                var imageBytes = await webClient.DownloadDataTaskAsync(uri);
+                blobBinding.Write(imageBytes);
+            }
+
+            log.Info($"Processed the download event image for identifier `{eventImageModel.Id}`");
         }
     }
 }
