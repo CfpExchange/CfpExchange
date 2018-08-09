b7d3fd794c41be89954733dcf1d32610ff143967
Jan de Vries <jan@jan-v.nl>
2018-08-09 20:26:59 +0200

    Moved shared model to shared NetStandard project Implemented function to download image to blob storage

--- CfpExchange.Functions/DownloadEventImage.cs
+++ CfpExchange.Functions/DownloadEventImage.cs
@@ -1,10 +1,13 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
 
 namespace CfpExchange.Functions
 {
@@ -13,24 +16,34 @@ namespace CfpExchange.Functions
         [FunctionName("DownloadEventImage")]
         public static async Task Run(
             [ServiceBusTrigger("eventimages", AccessRights.Manage, Connection = "ServicebusQueueConnectionString")]
-            Model.DownloadEventImage eventImageModel,
+            string eventImageMessage,
             Binder binder,
             TraceWriter log)
         {
+            var eventImageModel = JsonConvert.DeserializeObject<Models.DownloadEventImage>(eventImageMessage);
             log.Info($"Processing the download event image for identifier `{eventImageModel.Id}`");
 
-            Uri uri = new Uri(eventImageModel.ImageUrl);
-            var filename = Path.GetFileName(uri.LocalPath);
-            var downloadLocationForEventImage = $"event-images/{eventImageModel.Id}/{filename}";
-
-            using (var blobBinding = await binder.BindAsync<BinaryWriter>(
-                                                new BlobAttribute(downloadLocationForEventImage, FileAccess.Write)))
+            if (Uri.IsWellFormedUriString(eventImageModel.ImageUrl, UriKind.Absolute))
             {
-                var webClient = new WebClient();
-                var imageBytes = await webClient.DownloadDataTaskAsync(uri);
-                blobBinding.Write(imageBytes);
-            }
+                log.Verbose($"Event image URL is `{eventImageModel.ImageUrl}`.");
+                Uri uri = new Uri(eventImageModel.ImageUrl);
+                var filename = Path.GetFileName(uri.LocalPath);
+                var downloadLocationForEventImage = $"eventimages/{eventImageModel.Id}/{filename}";
 
+                using (var blobBinding = await binder.BindAsync<Stream>(
+                    new BlobAttribute(downloadLocationForEventImage, FileAccess.Write)))
+                {
+                    var webClient = new WebClient();
+                    var imageBytes = await webClient.DownloadDataTaskAsync(uri);
+                    log.Verbose($"Writing event image for CFP `{eventImageModel.Id}` to location `{downloadLocationForEventImage}`.");
+                    await blobBinding.WriteAsync(imageBytes, 0, imageBytes.Length);
+                }
+            }
+            else
+            {
+                log.Warning($"The location `{eventImageModel.ImageUrl}` for CFP `{eventImageModel.Id}` is not an absolute URI.");
+            }
+            
             log.Info($"Processed the download event image for identifier `{eventImageModel.Id}`");
         }
     }
