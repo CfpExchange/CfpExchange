fbe6a17fe191194ff376bc13f6da5bf6b1023db7
Yvo Linssen <yvo@dotcontrol.nl>
2018-08-10 14:37:00 +0200

    added less files etc.

--- Themes/gulpfile.js
+++ Themes/gulpfile.js
@@ -8,47 +8,32 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     prettify = require('gulp-html-prettify'),
     cleanhtml = require('gulp-cleanhtml'),
-    image = require('gulp-image'),
-    multiDest = require('gulp-multi-dest');
-
-// HTML task
-gulp.task('htmlclean', function() {
-    gulp.src('./grill/src/*.html')
-        .pipe(cleanhtml())
-        .pipe(prettify({
-            indent_char: ' ',
-            indent_size: 2
-        }))
-        .pipe(gulp.dest('./grill/dist/'))
-        .pipe(cleanhtml())
-        .pipe(gulp.dest('./grill/demo/'));
-
-});
+    image = require('gulp-image');
 
 // Less Build Task
 gulp.task('less', function() {
-    gulp.src('./grill/src/less/**/styles.less')
+    gulp.src('./CFPThemeV1/less/styles.less')
         .pipe(plumber())
         .pipe(less())
-        .pipe(gulp.dest('./grill/dist/css'))
+        .pipe(gulp.dest('../CfpExchange/wwwroot/css'))
         .pipe(cssmin())
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(multiDest(['./grill/dist/css', './grill/demo/css']))
+        .pipe(gulp.dest('../CfpExchange/wwwroot/css'));
 });
 
 //Javascript Task
 gulp.task('scripts', function() {
-    gulp.src('./grill/src/js/*.js')
+    gulp.src('./CFPThemeV1/js/*.js')
         .pipe(concat('scripts.js'))
         .pipe(uglify())
-        .pipe(multiDest(['./grill/dist/js', './grill/demo/js']));
+        .pipe(gulp.dest('../CfpExchange/wwwroot/js'));
 });
 
 // Image Task
 gulp.task('image', function() {
-    gulp.src('./grill/src/images/*')
+    gulp.src('./CFPExchange/img/*')
         .pipe(image({
             pngquant: true,
             optipng: false,
@@ -61,13 +46,12 @@ gulp.task('image', function() {
             svgo: true,
             concurrent: 10
         }))
-        .pipe(multiDest(['./grill/dist/images', './grill/demo/images']))
+        .pipe(gulp.dest('../CfpExchange/wwwroot/images'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./grill/src/**/*.html', ['htmlclean']);
-    gulp.watch('./grill/src/less/**/*.less', ['less']);
-    gulp.watch('./grill/src/js/*.js', ['scripts']);
+    gulp.watch('./CFPThemeV1/less/**/*.less', ['less']);
+    gulp.watch('./CFPThemeV1/js/*.js', ['scripts']);
 });
 
 gulp.task('default', ['watch']);
