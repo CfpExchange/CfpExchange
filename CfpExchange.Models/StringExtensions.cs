9e6c02a422e201c61a7ed009f4742cf855c66d27
Fanie Reynders <fanie@reynders.co>
2018-09-21 11:24:07 +0200

    fixes #71, @jandev had one job! (#74)
    * fixes #71, @jandev had one job!
    
    * check for noimage.svg. fixes #70
    
    * bringing data back.

---
+++ CfpExchange.Models/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace CfpExchange.Models
+{
+    public static class StringExtensions
+    {
+        public static bool IsDefaultImage(this string urlString)
+        {
+            return urlString.ToLower().EndsWith("noimage.svg");
+        }
+    }
+}
