c5485813e6c739edbbfb5e7c592b534b1ecbca06
Rick van den Bosch <rickvdbosch@outlook.com>
2020-02-26 16:57:09 +0100

    Cleanup and renamed GetEnvironmentVariable paramater

--- CfpExchange.Functions/Functions/BaseFunction.cs
+++ CfpExchange.Functions/Functions/BaseFunction.cs
@@ -4,9 +4,9 @@ namespace CfpExchange.Functions
 {
     public abstract class BaseFunction
     {
-        protected static string GetEnvironmentVariable(string name)
+        protected static string GetEnvironmentVariable(string settingName)
         {
-            return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+            return Environment.GetEnvironmentVariable(settingName, EnvironmentVariableTarget.Process);
         }
     }
 }
