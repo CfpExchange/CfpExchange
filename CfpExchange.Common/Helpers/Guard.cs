58f2a41e65a1e9af0626f438a4515ab36daae731
Rick van den Bosch <rickvdbosch@outlook.com>
2020-02-26 17:28:16 +0100

    Added Guard helper and setting helper to Common

---
+++ CfpExchange.Common/Helpers/Guard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CfpExchange.Common.Helpers
+{
+    public static class Guard
+    {
+        /// <summary>
+        /// Throws an <see cref="ArgumentNullException" /> when the given <paramref name="argumentValue" /> is null.
+        /// </summary>
+        /// <param name="argumentValue">The argument value.</param>
+        /// <param name="argumentName">Name of the argument.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Throws an <see cref="ArgumentNullException" /> when the specified <paramref name="argumentValue" /> is null.
+        /// </exception>
+        public static void IsNotNull(object argumentValue, string argumentName)
+        {
+            if (argumentValue == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+    }
+}
