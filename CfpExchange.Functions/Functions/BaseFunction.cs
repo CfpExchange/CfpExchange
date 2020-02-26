6f7e78dcb240f657405904d18732a3d92d309fcc
Rick van den Bosch <rickvdbosch@outlook.com>
2020-02-26 11:54:01 +0100

    Removed unnecessary Remove from  CfpExchange.csproj Reorganized Functions project

---
+++ CfpExchange.Functions/Functions/BaseFunction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CfpExchange.Functions
+{
+    public abstract class BaseFunction
+    {
+        protected static string GetEnvironmentVariable(string name)
+        {
+            return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
+    }
+}
