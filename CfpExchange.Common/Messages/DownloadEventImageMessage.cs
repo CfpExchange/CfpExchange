219a5af595caf871bb201639a935971d3a6652de
Rick van den Bosch <rickvdbosch@outlook.com>
2020-06-15 11:30:59 +0200

    Added messages to CfpExchange.Common.Messages

---
+++ CfpExchange.Common/Messages/DownloadEventImageMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CfpExchange.Common.Messages
+{
+    public class DownloadEventImageMessage
+    {
+        public Guid Id { get; set; }
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public bool HasDefaultImage()
+        {
+            return ImageUrl.EndsWith("noimage.svg", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
