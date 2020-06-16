8c8ed5a7e91fee2dd6dccaf286a977dc02e827d7
Rick van den Bosch <rickvdbosch@outlook.com>
2020-06-16 11:11:15 +0200

    Added CfpExchange.Common.UnitTests for Guard and SettingsHelper

---
+++ CfpExchange.Common.UnitTests/Helpers/GuardTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xunit;
+
+using CfpExchange.Common.Helpers;
+
+namespace CfpExchange.Common.UnitTests.Helpers
+{
+    public class GuardTests
+    {
+        #region Fields
+
+        private static object _nullValue = null;
+        private static object _nonNullValue = new object();
+
+        #endregion
+
+        [Fact]
+        public void IsNotNull_WithArgumentValueNull_ShouldThrowArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => Guard.IsNotNull(_nullValue, nameof(_nullValue)));
+
+            Assert.Equal(nameof(_nullValue), exception.ParamName);
+        }
+
+        [Fact]
+        public void IsNotNull_WithArgumentValueNotNull_ShouldNotThrowArgumentNullException()
+        {
+            Guard.IsNotNull(_nonNullValue, nameof(_nonNullValue));
+        }
+    }
+}
---
+++ CfpExchange.Common.UnitTests/Helpers/SettingsHelperTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using CfpExchange.Common.Helpers;
+using Xunit;
+
+namespace CfpExchange.Common.UnitTests.Helpers
+{
+    public class SettingsHelperTests
+    {
+        #region Constants
+
+        private const string EXISTING_SETTING = "Some Setting";
+        private const string EXISTING_SETTING_VALUE = "Some Value";
+        private const string NON_EXISTING_SETTING = "Pretty Sure This Setting Doesn't Exist 🤓";
+
+        #endregion
+
+        #region Constructors
+
+        public SettingsHelperTests()
+        {
+            Environment.SetEnvironmentVariable(EXISTING_SETTING, EXISTING_SETTING_VALUE);
+        }
+
+        #endregion
+
+        [Fact]
+        public void GetEnvironmentVariable_WithSettingNameNull_ShouldThrowArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => SettingsHelper.GetEnvironmentVariable(null));
+
+            Assert.Equal("settingName", exception.ParamName);
+        }
+
+        [Fact]
+        public void GetEnvironmentVariable_WithSettingNameExisting_ShouldReturnSettingValue()
+        {
+            var settingValue = SettingsHelper.GetEnvironmentVariable(EXISTING_SETTING);
+
+            Assert.Equal(EXISTING_SETTING_VALUE, settingValue);
+        }
+
+        [Fact]
+        public void GetEnvironmentVariable_WithSettingNameNotExisting_ShouldReturnNull()
+        {
+            var settingValue = SettingsHelper.GetEnvironmentVariable(NON_EXISTING_SETTING);
+
+            Assert.Null(settingValue);
+        }
+    }
+}
