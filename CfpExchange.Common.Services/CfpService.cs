20edd7f30fde76e28c0fb23ad3330b91fdea9efe
Rick van den Bosch <rickvdbosch@outlook.com>
2020-06-19 16:38:46 +0200

    Moved (I)CfpService to the Common.Services project

---
+++ CfpExchange.Common.Services/CfpService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using CfpExchange.Common.Data;
+using CfpExchange.Common.Models;
+using CfpExchange.Common.Services.Interfaces;
+
+namespace CfpExchange.Common.Services
+{
+    public class CfpService : ICfpService
+    {
+        private const int MaximumNumberOfItemsPerPage = 10;
+
+        private readonly CfpContext _cfpContext;
+
+        public CfpService(CfpContext cfpContext)
+        {
+            _cfpContext = cfpContext;
+        }
+
+        public Cfp GetCfpById(Guid id)
+        {
+            return _cfpContext.Cfps.SingleOrDefault(cfp => cfp.Id == id);
+        }
+
+        public Cfp GetCfpBySlug(string slug)
+        {
+            return _cfpContext.Cfps.SingleOrDefault(cfp => cfp.Slug == slug);
+        }
+
+        public List<Cfp> GetAllActiveCfps(string lowercaseSearchTerm, DateTime startDateTime, DateTime endDateTime, int pageToShow)
+        {
+            var allActiveCfps = _cfpContext.Cfps
+                .Where(cfp => cfp.CfpEndDate > DateTime.UtcNow)
+                .Where(cfp => cfp.DuplicateOfId == null)
+                .Where(cfp => cfp.EventName.ToLower().Contains(lowercaseSearchTerm)
+                              || cfp.EventLocationName.ToLower().Contains(lowercaseSearchTerm)
+                              || cfp.EventTags.ToLower().Contains(lowercaseSearchTerm))
+                .Where(cfp => cfp.EventStartDate == default || cfp.EventEndDate == default ||
+                              cfp.EventStartDate >= startDateTime && cfp.EventEndDate <= endDateTime)
+                .OrderBy(cfp => cfp.CfpEndDate)
+                .Skip((pageToShow - 1) * MaximumNumberOfItemsPerPage)
+                .Take(MaximumNumberOfItemsPerPage)
+                .ToList();
+
+            return allActiveCfps;
+        }
+
+        public List<Cfp> GetNewestActiveCfps(int pageToShow)
+        {
+            var allActiveCfps = _cfpContext.Cfps
+                .Where(cfp => cfp.CfpEndDate > DateTime.UtcNow)
+                .Where(cfp => cfp.DuplicateOfId == null)
+                .OrderByDescending(cfp => cfp.CfpAdded.Date)
+                .ThenBy(cfp => cfp.CfpEndDate.Date)
+                .Skip((pageToShow - 1) * MaximumNumberOfItemsPerPage)
+                .Take(MaximumNumberOfItemsPerPage)
+                .ToList();
+            return allActiveCfps;
+        }
+
+        public List<Cfp> IsAlreadyInDatabase(Uri parsedUri)
+        {
+            var alreadyInDatabase = _cfpContext.Cfps
+                .Where(cfp => cfp.CfpEndDate > DateTime.UtcNow)
+                .Where(c => c.EventUrl.ToLowerInvariant().Contains(parsedUri.Host) ||
+                            c.CfpUrl.ToLowerInvariant().Contains(parsedUri.Host))
+                .ToList();
+
+            return alreadyInDatabase;
+        }
+
+        public bool CfpWithIdenticalSlugExists(string cfpToAddSlug)
+        {
+            return _cfpContext.Cfps.Any(cfp => cfp.Slug == cfpToAddSlug);
+        }
+
+        public void AddCfp(Cfp cfpToAdd)
+        {
+            _cfpContext.Add(cfpToAdd);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _cfpContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ CfpExchange.Common.Services/Interfaces/ICfpService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using CfpExchange.Common.Models;
+
+namespace CfpExchange.Common.Services.Interfaces
+{
+    public interface ICfpService
+    {
+        Cfp GetCfpById(Guid id);
+
+        Cfp GetCfpBySlug(string slug);
+
+        List<Cfp> GetAllActiveCfps(string lowercaseSearchTerm, DateTime startDateTime, DateTime endDateTime, int pageToShow);
+
+        List<Cfp> GetNewestActiveCfps(int pageToShow);
+
+        List<Cfp> IsAlreadyInDatabase(Uri parsedUri);
+
+        bool CfpWithIdenticalSlugExists(string cfpToAddSlug);
+
+        void AddCfp(Cfp cfpToAdd);
+        
+        Task SaveChangesAsync();
+    }
+}
