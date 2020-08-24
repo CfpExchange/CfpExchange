f95502a2182f3ae9091ed34ba686ded5870599de
Rick van den Bosch <rickvdbosch@outlook.com>
2020-08-24 17:57:21 +0200

    Added initial CfpController

---
+++ CfpExchange.API/Controllers/CfpController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+using CfpExchange.API.Models;
+using CfpExchange.Common.Services.Interfaces;
+
+namespace CfpExchange.API.Controllers
+{
+    public class CfpController : ControllerBase
+    {
+        private ICfpService _cfpService;
+
+        public CfpController(ICfpService cfpService)
+        {
+            _cfpService = cfpService;
+        }
+
+        [HttpGet]
+        public IEnumerable<CfpData> Get()
+        {
+            // TODO implement
+            return null;
+        }
+
+        [HttpGet]
+        public CfpData GetById(Guid id)
+        {
+            // TODO implement
+            return null;
+        }
+    }
+}
---
+++ CfpExchange.API/Models/CfpData.cs
@@ -0,0 +1,7 @@
+﻿namespace CfpExchange.API.Models
+{
+    public class CfpData
+    {
+
+    }
+}
