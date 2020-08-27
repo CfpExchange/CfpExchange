f33ae1d9a64ee57780fa9953a3e1d464aa693be7
Rick van den Bosch <rickvdbosch@outlook.com>
2020-08-27 22:53:45 +0200

    Added intial properties to CfpData

--- CfpExchange.API/Models/CfpData.cs
+++ CfpExchange.API/Models/CfpData.cs
@@ -1,7 +1,13 @@
-﻿namespace CfpExchange.API.Models
+﻿using System;
+
+namespace CfpExchange.API.Models
 {
     public class CfpData
     {
+        public Guid Id { get; set; }
 
+        public string EventName { get; set; }
+
+        public string EventDescription { get; set; }
     }
 }
